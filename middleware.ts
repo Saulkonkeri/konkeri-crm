1f381a278bb255338ca8429e6df9e63b36bd3043
Saul Intriago <saulintriago@MacBook-Air-de-Saul.local>
2026-08-12 20:22:30 -0500

    Corrige ruta de importacion en el middleware

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,6 @@
 import { type NextRequest } from 'next/server';
-import { updateSession } from '@/utils/supabase/middleware';
+// Cambiamos @/ por ./ para que use una ruta relativa exacta
+import { updateSession } from './utils/supabase/middleware';
 
 export async function middleware(request: NextRequest) {
   return await updateSession(request);
