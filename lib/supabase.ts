85e6a44863fe0345553e0326a03a9ef8d65a5856
Saul Intriago <saulintriago@MacBook-Air-de-Saul.local>
2026-08-12 19:06:51 -0500

    Desactiva temporalmente el middleware

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,10 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
+import { createBrowserClient } from '@supabase/ssr';
 
-const supabaseUrl = 'https://ijzqqbybubruthargcnq.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlqenFxYnlidWJydXRoYXJnY25xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE5Nzg1ODYsImV4cCI6MjA5NzU1NDU4Nn0.bCcb-d_hcXFRrWOhu7-3RrmQUWaeIQSntllowRUyBm4';
-
-if (!supabaseUrl || !supabaseAnonKey) {
-  console.error('Faltan las variables de entorno de Supabase.');
-}
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createBrowserClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
