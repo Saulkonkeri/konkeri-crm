749ff3899c5eb40246f98c081445424b1deae395
Saul Intriago <saul@konkeri.com>
2026-08-31 18:27:43 -0500

    Corrige importacion de React

--- app/(sistema)/radar/page.tsx
+++ app/(sistema)/radar/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { supabase } from '@/lib/supabase';
 
 // Tipo de dato para agrupar las sesiones
