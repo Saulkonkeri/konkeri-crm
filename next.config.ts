b779c158ddc628207bfd0b17d38efefa1e6af4d8
Saul Intriago <saul@konkeri.com>
2026-09-08 18:25:24 -0500

    Agregando redireccion web

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,31 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        // Regla para el dominio principal (Landing)
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'arienzoliving.com',
+          },
+        ],
+        destination: '/inicio/:path*',
+      },
+      {
+        // Regla para el dominio con www (Landing)
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.arienzoliving.com',
+          },
+        ],
+        destination: '/inicio/:path*',
+      }
+    ];
+  },
+};
 
 export default nextConfig;
