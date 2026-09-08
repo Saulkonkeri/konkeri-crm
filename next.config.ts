a595a0d9a674522923f949494fa1ea501fe568a2
Saul Intriago <saul@konkeri.com>
2026-09-08 18:50:52 -0500

    Ajuste de prioridad beforeFiles

--- next.config.ts
+++ next.config.ts
@@ -2,31 +2,34 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   async rewrites() {
-    return [
-      {
-        // Regla para el dominio principal
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'arienzoliving.com',
-          },
-        ],
-        destination: '/inicio/:path*',
-      },
-      {
-        // Regla para el www
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'www.arienzoliving.com',
-          },
-        ],
-        destination: '/inicio/:path*',
-      },
-    ];
+    return {
+      beforeFiles: [
+        {
+          // Regla de máxima prioridad para el dominio principal
+          source: '/:path*',
+          has: [
+            {
+              type: 'host',
+              value: 'arienzoliving.com',
+            },
+          ],
+          destination: '/inicio/:path*',
+        },
+        {
+          // Regla de máxima prioridad para el www
+          source: '/:path*',
+          has: [
+            {
+              type: 'host',
+              value: 'www.arienzoliving.com',
+            },
+          ],
+          destination: '/inicio/:path*',
+        },
+      ],
+    };
   },
 };
 
 export default nextConfig;
+
